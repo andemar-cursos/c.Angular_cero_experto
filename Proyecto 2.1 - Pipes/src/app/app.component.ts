db0543c6903421506ff9993fdf27c248a92f2456
Anderson <andemartinezdev@gmail.com>
2020-10-24 18:46:17 -0500

    Clase 5-7 -> Percent
    - Se uso el percent el cual muestra un numero (0.234) en porcentaje (0.23%).

--- Proyecto 2.1 - Pipes/src/app/app.component.ts
+++ Proyecto 2.1 - Pipes/src/app/app.component.ts
@@ -10,4 +10,5 @@ export class AppComponent {
   nombre: string = 'Capitan America';
   arreglo: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   PI: number = Math.PI;
+  porcentaje: number = 0.234;
 }
