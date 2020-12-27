78befb6d38c1157634937ee5a246d4ecdcf8ce90
Anderson <andemartinezdev@gmail.com>
2020-12-27 17:11:04 -0500

    Clase 5-11 -> Pipe Date
    - Se uso el pipe Date y se cambio el idioma por defecto al espanol.

--- Proyecto 2.1 - Pipes/src/app/app.module.ts
+++ Proyecto 2.1 - Pipes/src/app/app.module.ts
@@ -1,7 +1,11 @@
+import { registerLocaleData } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import localEs from '@angular/common/locales/es'
+
+registerLocaleData(localEs)
 
 @NgModule({
   declarations: [
@@ -10,7 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'es' } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
