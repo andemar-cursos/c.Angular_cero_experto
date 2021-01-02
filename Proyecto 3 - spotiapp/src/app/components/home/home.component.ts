ea3bc7d2e41d0b64e9fa666c13c7429341424aa0
Anderson <andemartinezdev@gmail.com>
2021-01-02 16:07:18 -0500

    Clase 6-8 -> Token de spotify
    - Se reviso como obtener el token de spotify (7bdcfb60b78d48bda65414c98babc61d).
    - Se borro las lineas de codigo de prueba de la clase anterior.

--- Proyecto 3 - spotiapp/src/app/components/home/home.component.ts
+++ Proyecto 3 - spotiapp/src/app/components/home/home.component.ts
@@ -1,24 +1,13 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class HomeComponent implements OnInit {
 
-  paises: any[] = [];
-
-  constructor( private http: HttpClient ) { 
-
-    this.http.get('https://restcountries.eu/rest/v2/lang/es')
-      .subscribe( (res: any) => {
-        this.paises = res;
-        console.log(res);
-      });
-  }
+  constructor() { }
 
   ngOnInit(): void {
   }
