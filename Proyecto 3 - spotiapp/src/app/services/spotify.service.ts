9bd9bf611e6fc092885fb0edbf1b59b875a9c11f
Anderson <andemartinezdev@gmail.com>
2021-01-03 14:09:00 -0500

    Clase 6-11 -> Busqueda de artistas
    - Se construyo el componente que busca los artistas.

--- Proyecto 3 - spotiapp/src/app/services/spotify.service.ts
+++ Proyecto 3 - spotiapp/src/app/services/spotify.service.ts
@@ -6,16 +6,30 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class SpotifyService {
 
+  bearer: string = "BQCOGAE9k5BlsjAMJQlcEOwpqYxEDMn0Jvvt2DADjnz_rMQb_DXtO_2GIoXOM-fA3h4KdA1MrknejsUvfUc";
+
   constructor( private htpp: HttpClient) { }
 
 
   getNewReleases() {
     
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQB1nc7M7H2fC3IU2VxCbyh7NpAcKEJvnr1EGPW25pvdDo0oMStWYCfu0ym8oMyIiYELtYoNAyQ-B1QBloM',
+      'Authorization': `Bearer ${this.bearer}`,
 
     });
 
     return this.htpp.get('https://api.spotify.com/v1/browse/new-releases', {headers});
   }
+
+
+  getArtista(termino: string) {
+    
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.bearer}`,
+
+    });
+
+    return this.htpp.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers});
+  }
+
 }
