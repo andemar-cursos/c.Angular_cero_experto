c87e0d214c7c76adb8f7b1481196d14af15a35de
Anderson <andemartinezdev@gmail.com>
2021-01-23 20:23:14 -0500

    Clase 7-20 -> Compoente lista
    - Se diseno el componente.
    - Se configuro las rutas, los cuales llaman a este componente.

--- Proyecto 4 - deseos/src/app/components/listas/listas.component.ts
+++ Proyecto 4 - deseos/src/app/components/listas/listas.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DeseosService } from '../../services/deseos.service';
+import { Lista } from '../../models/lista.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listas',
@@ -7,8 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListasComponent implements OnInit {
 
-  constructor() { }
+  @Input() terminada = true;
+
+  constructor(  public deseoService: DeseosService,
+                private router       : Router) { }
 
   ngOnInit() {}
 
+
+  listaSeleccionada(lista: Lista) {
+
+    (this.terminada)  ? this.router.navigateByUrl(`/tabs/tab2/agregar/${lista.id}`)
+                      : this.router.navigateByUrl(`/tabs/tab1/agregar/${lista.id}`);
+    
+  }
 }
